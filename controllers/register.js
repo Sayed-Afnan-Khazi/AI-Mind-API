a347577002b76f14b08f3d011d6254bc58f11953
Sayed Afnan Khazi <83779299+Sayed-Afnan-Khazi@users.noreply.github.com>
2024-12-16 19:33:29 +0530

    Testing env bug

--- controllers/register.js
+++ controllers/register.js
@@ -4,8 +4,9 @@ const handleRegister = (req,res,db,bcrypt) => {
 		return;
 	}
 	const hash = bcrypt.hashSync(req.body.password,8); // 8 salt rounds
-	console.log("Registering")
+	console.log("Registering",req.body.email,req.body.name,hash);
 	db.transaction(trx=>{
+		console.log("Inserting into login");
 		trx.insert({
 			hash: hash,
 			email: req.body.email
@@ -20,14 +21,15 @@ const handleRegister = (req,res,db,bcrypt) => {
 			})
 			.into('users')
 			.returning('*')
-			.then(user=>res.status(200).json(user[0]))
-			.catch(err=>res.status(400).json('Unable to register.'))
+			.then(user=>{console.log(user);res.status(200).json(user[0])})
+			// .catch(err=>res.status(400).json(`Unable to register.${err}`))
+			.catch(err=>console.log(err))
 		})
 		.then(trx.commit)
 		.catch(trx.rollback)
 	})
-	.catch(err=>res.status(400).json('Unable to register.'))
-	// .catch(console.log)
+	// .catch(err=>res.status(400).json(`Unable to register.${err}`))
+	.catch(console.log)
 }
 
 module.exports = {
